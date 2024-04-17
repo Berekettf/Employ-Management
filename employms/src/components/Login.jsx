0e1cea0a00b5ba87747c67d71ede601fb0e57977
BereketWU <BereketWU>
2024-04-17 22:43:10 +0300

    first commit

--- employms/src/components/Login.jsx
+++ employms/src/components/Login.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './style.css'
+import axios from 'react-router-dom'
+
 
 
 const Login = () => {
+
+    const [values, setValues] = useState({
+        email: '',
+        password:''
+    })
+    const handleSubmit = (e)=>{
+        e.preventDefult()
+        axios.post('http://localhost:8000/auth/login', {email, password})
+        .then(result=>console.log(result))
+        .catch(error=>console.log(error))
+    }
 
 
   return (
     <div className='d-flex justify-content-center align-items-center vh-100 loginPage'>
         <div className='p-3 rounded w-25 border loginForm'>
             <h2>Login Page</h2>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div className='mb-3'>
                     <label htmlFor="email"><strong>Email:</strong></label>
-                    <input type="email" name='email' autoComplete='off' placeholder='Enter Email' className='form-control rounded-0'/>
+                    <input type="email" name='email' autoComplete='off' placeholder='Enter Email' 
+                    onClick={(e)=>setValues({...values, email: e.target.value})}  className='form-control rounded-0'/>
                 </div>
                 <div className='mb-3'> 
                     <label htmlFor="password"><strong>Password:</strong></label>
-                    <input type="password" name='password' placeholder='Enter Password' className='form-control rounded-0'/>
+                    <input type="password" name='password' placeholder='Enter Password' 
+                    onClick={(e)=>setValues({...values, email: e.target.value})} className='form-control rounded-0'/>
                 </div>
                 <button className='btn btn-success w-100 rounded-0 mb-2'>Log in</button>
                 <div className='mb-1'> 
